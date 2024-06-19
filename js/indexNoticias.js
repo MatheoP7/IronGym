214430ead61e2e1f57c1243b24d3e1d3c811b03b
MatheoP7 <matheomtp@icloud.com>
2024-06-19 15:02:55 +0200

    Mi tercer commit

--- js/indexNoticias.js
+++ js/indexNoticias.js
@@ -1,5 +1,5 @@
 document.addEventListener("DOMContentLoaded", () => {
-  fetch('noticias.json')
+  fetch('../json/noticias.json')
       .then(response => {
           if (!response.ok) {
               throw new Error('Network response was not ok');
